f55624ab684f3fa6b3e1a55b98a4157da0ad5e81
Devansh <guyawsm@gmail.com>
2024-02-09 22:58:22 +0530

    added report

--- src/App.js
+++ src/App.js
@@ -9,18 +9,6 @@ import Report from './Components.js/Report';
 function App() {
   const [formamount, setAmount] = useState(false)
   const [updateHistory, setUpdateHistory] = useState(false)
-  useEffect(()=>{
-    var history = JSON.parse(localStorage.getItem('history'))
-    var his = []
-    history.forEach(element => {
-      if(history.indexOf(element)<5)
-        element['date']="8/2/2024"
-      else
-        element['date']="9/2/2024"
-      his.push(element)
-    });
-    localStorage.setItem('history', JSON.stringify(his))
-  },[])
   return (
     <div className="App">
       <img src='https://www.clipartmax.com/png/full/36-365828_navbar-toggle-icon-menu-hamburger-png-white.png' style={{width:'40px', margin:'20px'}} onClick={()=>{setAmount(!formamount)}}></img>
@@ -28,7 +16,7 @@ function App() {
       {!formamount && <AmountLeft updateHistory={updateHistory}/>}
       {!formamount && <Deduct setUpdateHistory={setUpdateHistory} updateHistory={updateHistory}/>}
       {!formamount && <Record updateHistory={updateHistory}/>}
-      {/* {!formamount && <Report updateHistory={updateHistory}/>} */}
+      {!formamount && <Report updateHistory={updateHistory}/>}
     </div>
   );
 }
