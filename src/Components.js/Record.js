483a4c5d0343c1baa8a72778e32cd0991191fa85
Devansh Abrol <80891722+devansh1503@users.noreply.github.com>
2024-02-15 12:16:48 +0530

    Update Record.js

--- src/Components.js/Record.js
+++ src/Components.js/Record.js
@@ -17,13 +17,14 @@ function Record(props) {
     <div style={{margin:'30px'}}>
         <h1 style={{fontSize:'30px', color:'white'}}>History</h1>
         {his ? his.slice(his.length-4, his.length).map((item)=>{
-            return <div style={{display:'flex', backgroundColor:'#4308E5', alignItems:'center', padding:'10px', marginTop:'10px'}}>
+            return <div style={{display:'flex', justifyContent:'space-between', alignItems:'center', backgroundColor:'#4308E5', alignItems:'center', padding:'10px', marginTop:'10px'}}>
                 {/* <img src='https://static.vecteezy.com/system/resources/previews/009/267/401/original/minus-sign-icon-free-png.png' 
                 style={{width:'40px', height:'40px'}}></img> */}
                 <div style={historyStyle}>
                     <div>Rs.{item.amount}</div>
                     <div style={{color:'#fc466b'}}>{item.reason}</div>
                 </div>
+                <div style={{fontSize:'20px', color:'white'}}>{item.date}</div>
             </div>
         }): <div/>}
     </div>
